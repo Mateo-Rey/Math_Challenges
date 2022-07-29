488fd5a04c892b0e6985f35e515092e938bbcd55
Mateo Rey <mateorey@Mateos-MacBook-Air.local>
2022-07-29 08:52:33 -0400

    fixed twosum

--- fast_twosum.js
+++ fast_twosum.js
@@ -4,15 +4,17 @@ let array = [2, 6, 3, 1];
 function fastTwoSum(arr, target) {
   let map = {
   };
-
+  let res = []
   for (let i = 0; i < arr.length; i++) {
     let difference = target - arr[i];
     if (map[difference] !== undefined) {
-      console.log(arr[map[difference]], "+", arr[i], "=", target); // arr[1] + arr[3] = target
+      res.push(arr[map[difference]])
+      res.push(arr[i]); // arr[1] + arr[3] = target
     } else {
       map[arr[i]] = i;
     }
   }
+  return res;
 }
 
-fastTwoSum(array, target);
+console.log(fastTwoSum(array, target));
